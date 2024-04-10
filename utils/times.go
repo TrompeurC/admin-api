ac0c7187f494471c688476bb9b67c498b5eabac1
TrompeurC <13604811047@163.com>
2024-04-10 21:39:54 +0800

    feat: bug修复

--- utils/times.go
+++ utils/times.go
@@ -13,14 +13,14 @@ type HTime struct {
 var formatTime = "2006-01-02 13:04:05"
 
 func (t HTime) MarshalJSON() ([]byte, error) {
-	formatted := t.Format(formatTime)
+	formatted := fmt.Sprintf("\"%s\"", t.Format(formatTime))
 	return []byte(formatted), nil
 }
-
 func (t *HTime) UnmarshalJSON(data []byte) (err error) {
-	now, _ := time.Parse(formatTime, string(data))
+	now, err := time.ParseInLocation(`"`+formatTime+`"`, string(data),
+		time.Local)
 	*t = HTime{Time: now}
-	return err
+	return
 }
 func (t HTime) Value() (driver.Value, error) {
 	var zeroTime time.Time
@@ -30,10 +30,11 @@ func (t HTime) Value() (driver.Value, error) {
 	return t.Time, nil
 }
 
-func (t *HTime) Scan(v any) error {
+func (t *HTime) Scan(v interface{}) error {
 	value, ok := v.(time.Time)
 	if ok {
 		*t = HTime{Time: value}
+		return nil
 	}
 	return fmt.Errorf("can not convert %v to timestamp", v)
 }
